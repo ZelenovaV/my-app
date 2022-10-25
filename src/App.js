a51efc01b3b648051562fdceff498a20d2f73099
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-10-25 15:13:40 +0300

    add url on github

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import "./index.css";
 import Search from "./Search.js";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather forecast</h1>
-      <div className="container_maim">
-        {" "}
-        <Search />
+    <>
+      <div className="App">
+        <h1>Weather forecast</h1>
+        <div className="container_maim">
+          {" "}
+          <Search />
+        </div>
       </div>
-    </div>
+      <div>
+        <p>
+          Open-source code by{" "}
+          <a href="https://github.com/ZelenovaV/my-app" target="_blank">
+            ZelenovaV
+          </a>
+        </p>
+      </div>
+    </>
   );
 }
